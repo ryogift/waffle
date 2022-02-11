6a7ad5bbb6c9c1219f0d6e95e0f2c6783621eea5
Ryo Takahashi <ryogiftjp@gmail.com>
2022-02-11 19:23:46 +0900

    Fix function to arrow

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Sample from "./Sample";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Learn React</h1>
-      <Sample />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1>Learn React</h1>
+    <Sample />
+  </div>
+);
 
 export default App;
--- src/Sample.js
+++ src/Sample.js
@@ -1,16 +1,14 @@
 import { Container, Row, Col } from "react-bootstrap";
 
-function Sample() {
-  return (
-    <div className="Sample">
-      <Container>
-        <Row>
-          <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
-        </Row>
-      </Container>
-    </div>
-  );
-}
+const Sample = () => (
+  <div className="Sample">
+    <Container>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+    </Container>
+  </div>
+);
 
 export default Sample;
